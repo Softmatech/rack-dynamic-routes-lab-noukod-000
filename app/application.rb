e1aeafb1011c54c5388c6d618b1effc6b919dd6c
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-14 22:13:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,10 +12,10 @@ class Application
     elsif
       req.path != "/items"
 
-      resp.write "Item not found"
+      resp.write "Route not found"
       resp.status = 404
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 400
     end
     resp.finish
