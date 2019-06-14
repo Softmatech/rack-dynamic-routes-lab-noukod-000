28792a6929e5cca7721334f1fcc8e20af2b2bec2
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-14 22:17:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,10 @@
 class Application
 
-  attr_accessor :price
+  @@items = []
 
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path == "/items"
-      # resp.write "You requested the Items"
-      @price
-    elsif
-      req.path != "/items"
-
-      resp.write "Route not found"
-      resp.status = 404
-    else
-      resp.write "Item not found"
-      resp.status = 400
-    end
-    resp.finish
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
   end
-
+  
 end
